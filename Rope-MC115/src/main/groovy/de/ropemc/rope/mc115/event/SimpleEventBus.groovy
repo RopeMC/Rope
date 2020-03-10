bdc67628bdb48803ab908e8cf1d9c3d259841f98
x7airworker <jake@jakes-co.de>
2020-03-10 13:57:20 +0100

    Deleted @override

--- Rope-MC115/src/main/groovy/de/ropemc/rope/mc115/event/SimpleEventBus.groovy
+++ Rope-MC115/src/main/groovy/de/ropemc/rope/mc115/event/SimpleEventBus.groovy
@@ -8,7 +8,6 @@ import java.util.function.Consumer
 class SimpleEventBus implements EventBus {
     private Map<Class, List<Consumer>> subscriptions = [:]
 
-    @Override
     def <T> void subscribe(Class<T> to, Consumer<T> with) {
         if (!subscriptions.containsValue(to)) {
             subscriptions[to] = []
